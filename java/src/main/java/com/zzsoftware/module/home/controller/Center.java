6d61b1080c09a28c64136bf61fb4b74a1b1d7de2
6byte <444891953@qq.com>
2021-02-24 11:41:52 +0800

    02-24

--- java/src/main/java/com/zzsoftware/module/home/controller/Center.java
+++ java/src/main/java/com/zzsoftware/module/home/controller/Center.java
@@ -7,20 +7,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.zzsoftware.module.home.bean.Test;
 import com.zzsoftware.module.home.service.MyService;
+import com.zzsoftware.utils.genreator.Generator;
+import com.zzsoftware.utils.genreator.TableNamesDao;
 
 @RestController
 @CrossOrigin
 public class Center {
 	@Autowired
 	MyService MyService;
-	@GetMapping("test")
-	public String test() {
-		System.out.println( "获取" );
-		return "test";
-	}
+	
+	@Autowired
+	TableNamesDao tableNamesDao;
 	
 	@GetMapping("daoTest")
 	public List<Test> daoTest() {
 		return MyService.select();
+	}
+	
+//	生成代码
+	@GetMapping("exec")
+	public void exec(String []tableName,String moduleName,String author) {
+		Generator.exec(author,moduleName,tableName);
+	}
+	@GetMapping("findTableNames")
+	public List<String> findTableNames() {
+		return tableNamesDao.findTableNames();
 	}
 }
